3f7de0e802210bb97cc37ad9f45b08113d7434e5
daladim <daladim@users.noreply.github.com>
2021-03-28 19:44:20 +0200

    Added placeholder for settings.rs

---
+++ src/settings.rs
@@ -0,0 +1,9 @@
+// TODO: change these values with yours
+
+pub const URL: &str = "https://my.server.com/remote.php/dav/files/john";
+pub const USERNAME: &str = "username";
+pub const PASSWORD: &str = "secret_password";
+
+pub const EXAMPLE_TASK_URL: &str =     "https://my.server.com/remote.php/dav/calendars/john/6121A0BE-C2E0-4F16-A3FA-658E54E7062A/74439558-CDFF-426C-92CD-ECDDACE971B0.ics";
+pub const EXAMPLE_CALENDAR_URL: &str = "https://my.server.com/remote.php/dav/calendars/john/a_calendar_name/";
+
