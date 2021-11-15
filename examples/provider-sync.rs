91966b9d5ce2e4f52c5f43f99c39e842bd1a2c5c
daladim <daladim@users.noreply.github.com>
2021-11-15 22:21:55 +0100

    [minor] Format

--- examples/provider-sync.rs
+++ examples/provider-sync.rs
@@ -24,9 +24,9 @@ pub const URL: &str = "https://my.server.com/remote.php/dav/files/john";
 pub const USERNAME: &str = "username";
 pub const PASSWORD: &str = "secret_password";
 
-pub const EXAMPLE_TASK_URL: &str =     "https://my.server.com/remote.php/dav/calendars/john/6121A0BE-C2E0-4F16-A3FA-658E54E7062A/74439558-CDFF-426C-92CD-ECDDACE971B0.ics";
+pub const EXAMPLE_TASK_URL: &str =              "https://my.server.com/remote.php/dav/calendars/john/6121A0BE-C2E0-4F16-A3FA-658E54E7062A/74439558-CDFF-426C-92CD-ECDDACE971B0.ics";
 pub const EXAMPLE_EXISTING_CALENDAR_URL: &str = "https://my.server.com/remote.php/dav/calendars/john/a_calendar_name/";
-pub const EXAMPLE_CREATED_CALENDAR_URL: &str = "https://my.server.com/remote.php/dav/calendars/john/a_calendar_that_we_have_created/";
+pub const EXAMPLE_CREATED_CALENDAR_URL: &str =  "https://my.server.com/remote.php/dav/calendars/john/a_calendar_that_we_have_created/";
 
 
 
